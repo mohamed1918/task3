3d74921dad0a65d47d45e2a521007fbcde5a03ba
mohamed1918 <memoohossam2004@gmail.com>
2025-06-29 18:44:25 +0300

    Q2

--- task3/Program.cs
+++ task3/Program.cs
@@ -5,18 +5,31 @@
         static void Main(string[] args)
         {
             #region Q1
-            int num;
-            Console.WriteLine("Enter a number : ");
-            bool isnumber = int.TryParse(Console.ReadLine(), out  num);
-            if (num % 3 == 0 && num % 4 == 0)
-            {
-                Console.WriteLine("yes");
-            }
-            else
-            {
-                Console.WriteLine("No");
-            }
+            //int num;
+            //Console.WriteLine("Enter a number : ");
+            //bool isnumber = int.TryParse(Console.ReadLine(), out  num);
+            //if (num % 3 == 0 && num % 4 == 0)
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+
             #endregion
-        }
+            #region Q2
+           Console.WriteLine("Enter a Number : ");
+           int num;
+           bool isnumber = int.TryParse(Console.ReadLine(), out num);
+           if (num > 0)
+           {
+                Console.WriteLine("Number Is Postive");
+           }
+           else
+           {
+                Console.WriteLine("Number Is Negative");
+           }
+        #endregion
     }
+}
 }
