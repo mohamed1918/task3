6cb56ddb21a44b9874aab46adc8ee012d0606e58
mohamed1918 <memoohossam2004@gmail.com>
2025-06-29 18:46:50 +0300

    Q3

--- task3/Program.cs
+++ task3/Program.cs
@@ -18,18 +18,32 @@
 
             #endregion
             #region Q2
-           Console.WriteLine("Enter a Number : ");
-           int num;
-           bool isnumber = int.TryParse(Console.ReadLine(), out num);
-           if (num > 0)
-           {
-                Console.WriteLine("Number Is Postive");
-           }
-           else
-           {
-                Console.WriteLine("Number Is Negative");
-           }
-        #endregion
+            //Console.WriteLine("Enter a Number : ");
+            //int num;
+            //bool isnumber = int.TryParse(Console.ReadLine(), out num);
+            //if (num > 0)
+            //{
+            //     Console.WriteLine("Number Is Postive");
+            //}
+            //else
+            //{
+            //     Console.WriteLine("Number Is Negative");
+            //}
+            #endregion
+            #region Q3
+            Console.Write("Enter first number: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            int n3 = int.Parse(Console.ReadLine());
+            int max = Math.Max(n1, Math.Max(n2, n3));
+            int min = Math.Min(n1, Math.Min(n2, n3));
+            Console.WriteLine($"Max number = {max}");
+            Console.WriteLine($"Min number = {min}");
+
+            #endregion
+
+        }
     }
-}
 }
